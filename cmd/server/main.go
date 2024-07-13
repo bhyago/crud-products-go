03b6ef6e3aa69766400f7ad40fd476c1b0c7a205
Hyago Mesquita <bhyagomesquita@gmail.com>
2024-07-13 13:06:40 -0300

    add jwt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,46 @@
 package main
 
-import "github.com/bhyago/crud-products-go/configs"
+import (
+	"net/http"
+
+	"github.com/bhyago/crud-products-go/configs"
+	"github.com/bhyago/crud-products-go/internal/entity"
+	"github.com/bhyago/crud-products-go/internal/infra/database"
+	"github.com/bhyago/crud-products-go/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBHost)
+	configs, err := configs.LoadConfig(".")
+
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	ProductHandle := handlers.NewProductHandle(productDB)
+
+	userDB := database.NewUser(db)
+	userHandle := handlers.NewUserHandle(userDB, configs.TokenAuthKey, configs.JWTExpiresIn)
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Post("/products", ProductHandle.CreateProduct)
+	router.Get("/products/{id}", ProductHandle.GetProduct)
+	router.Get("/products", ProductHandle.GetProducts)
+	router.Put("/products/{id}", ProductHandle.UpdateProduct)
+	router.Delete("/products/{id}", ProductHandle.DeleteProduct)
+
+	router.Post("/users", userHandle.CreateUser)
+	router.Post("/users/generate_token", userHandle.GetJWT)
+
+	http.ListenAndServe(":3333", router)
 }
